218107cb47d117572ca9751da7c63477300e2f09
wanyimu <13307130174@fudan.edu.cn>
2015-08-23 15:32:37 +0800

    Ex1 Vectorized Optimization
    Use a vectorized way to calculate.

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,11 +16,15 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-    theta_temp = zeros(size(X, 2), 1);      % synchronized
-    for i = 1 : size(theta)     % here i means the num of features
-        theta_temp(i) = theta(i) - alpha * (1 / m) * sum((X * theta - y).* X(:,i));
-    end
-    theta = theta_temp;
+    
+    %   a vectorized way
+    theta = theta - alpha * (1/m) * ((X*theta - y)' * X)'; 
+    
+    %   theta_temp = zeros(size(X, 2), 1);      % synchronized
+    %   for i = 1 : size(theta)     % here i means the num of features
+    %   theta_temp(i) = theta(i) - alpha * (1 / m) * sum((X * theta - y).* X(:,i));
+    %   end
+    %   theta = theta_temp;
 
 
 
