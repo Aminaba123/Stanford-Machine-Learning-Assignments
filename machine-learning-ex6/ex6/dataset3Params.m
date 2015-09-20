0f4f06fa2dc1f6551f983dd3278bc1822fe006b0
wanyimu <13307130174@fudan.edu.cn>
2015-09-20 19:16:19 +0800

    Ex_6 Support Vector Machines
    Completed assignment for ex6.
    Fix a bug about visualization in the function visualizeBoundary.

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,10 +22,24 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
-
-
-
+Candidate = [0.01 0.03 0.1 0.3 1 3 10 30];
+best_C = 1;
+best_sigma = 0.3;
+max_accuracy = 0;
+for C = Candidate
+    for sigma = Candidate
+        model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        accuracy = mean(double(predictions == yval));
+        if (accuracy > max_accuracy)
+            max_accuracy = accuracy;
+            best_C = C;
+            best_sigma = sigma;
+        end
+    end
+end
+C = best_C;
+sigma = best_sigma;
 
 
 
